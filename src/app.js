89609681a5d62fd9aa7fffb164fa0a494358b37d
Maxi Fabbri <maxifabbri1972@gmail.com>
2025-05-14 15:44:36 -0300

    Nueva version de CORS

--- src/app.js
+++ src/app.js
@@ -15,13 +15,26 @@ const app = express();
 const PORT = process.env.PORT||8080;
 
 // CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://quattrum-cotizador.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  
+  if (req.method === "OPTIONS") {
+      return res.sendStatus(200); // Responde correctamente a preflight requests
+  }
+  
+  next();
+});
+
+// Configuración de CORS
 const corsOptions = {
   origin: ['http://localhost:5173', 'https://quattrum-cotizador.vercel.app'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 };
 app.use(cors(corsOptions));
+
 
 
 const ready = () => {
