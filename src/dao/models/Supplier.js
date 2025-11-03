8ada6c0a820766052e8a7bd1b386df99f29730cf
Maxi Fabbri <maxifabbri1972@gmail.com>
2025-11-03 15:43:00 -0300

    suppliers note added

--- src/dao/models/Supplier.js
+++ src/dao/models/Supplier.js
@@ -4,34 +4,39 @@ import mongoosePaginate from 'mongoose-paginate-v2';
 const collection = 'Suppliers';
 
 const schema = new mongoose.Schema({
-    name:{
+    name: {
         type: String,
-        required:true,
-        unique:true
+        required: true,
+        unique: true
     },
-    code:{
-        type:String,
-        required:false
+    code: {
+        type: String,
+        required: false
     },
-    cuit:{
-        type:String,
-        required:false
+    cuit: {
+        type: String,
+        required: false
     },
-    email:{
-        type:String,
-        required:false,
+    email: {
+        type: String,
+        required: false,
     },
-    phone:{
-        type:String,
-        required:false
+    phone: {
+        type: String,
+        required: false
     },
-    supplierPaymentMethodId:{
+    supplierPaymentMethodId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'SupplierPaymentMethods'
+    },
+    note: {
+        type: String,
+        required: false,
+        trim: true
     }
 })
 
 schema.plugin(mongoosePaginate);
-const supplierModel = mongoose.model(collection,schema);
+const supplierModel = mongoose.model(collection, schema);
 
 export default supplierModel;
