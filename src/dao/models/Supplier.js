1695e4d453fbea74bc6bab2b123b1c27f3640ffd
Maxi Fabbri <maxifabbri1972@gmail.com>
2026-05-14 17:23:18 -0300

    Done Add Suppliers Contacts

--- src/dao/models/Supplier.js
+++ src/dao/models/Supplier.js
@@ -4,37 +4,49 @@ import mongoosePaginate from 'mongoose-paginate-v2';
 const collection = 'Suppliers';
 
 const schema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    code: {
-        type: String,
-        required: false
-    },
-    cuit: {
-        type: String,
-        required: false
-    },
-    email: {
-        type: String,
-        required: false,
-    },
-    phone: {
-        type: String,
-        required: false
-    },
-    supplierPaymentMethodId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'SupplierPaymentMethods'
-    },
-    supplierNote: {
-        type: String,
-        required: false,
-        trim: true
-    }
-})
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  code: {
+    type: String,
+    required: false,
+  },
+  cuit: {
+    type: String,
+    required: false,
+  },
+  email: {
+    type: String,
+    required: false,
+  },
+  phone: {
+    type: String,
+    required: false,
+  },
+  supplierPaymentMethodId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "SupplierPaymentMethods",
+  },
+  supplierNote: {
+    type: String,
+    required: false,
+    trim: true,
+  },
+  supplierContact: {
+    type: [
+      {
+        id: String,
+        name: String,
+        position: String,
+        email: String,
+        phone: String,
+      },
+    ],
+    required: false,
+  }
+});
 
 schema.plugin(mongoosePaginate);
 const supplierModel = mongoose.model(collection, schema);
