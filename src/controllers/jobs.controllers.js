d59b9024ee0c3761a5832afa94d161c8fa2c11a8
Maxi Fabbri <maxifabbri1972@gmail.com>
2026-05-21 11:32:04 -0300

    Fixed jobs sort order

--- src/controllers/jobs.controllers.js
+++ src/controllers/jobs.controllers.js
@@ -83,7 +83,7 @@ async function readJobPopulatedPaginated(req, res) {
   const options = {
     page: parseInt(page) || 1,
     limit: parseInt(limit) || 50,
-    sort: { approvalDate: -1 },
+    sort: { approvalDate: -1, _id: -1 },
   };
 
 
